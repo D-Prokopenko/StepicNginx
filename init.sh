3ff6fec6dcd40347366c83d9139ade03a219b262
D-Prokopenko <denis1489@list.ru>
2022-11-15 23:07:30 +0300

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
-gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
