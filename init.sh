9fc0b5ae4c9dd167acbbf7c8e64a9880827f8e32
D-Prokopenko <denis1489@list.ru>
2022-11-15 17:08:39 +0300

    "hello" app

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo mv /home/box/StepicNginx/web ~
 sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
