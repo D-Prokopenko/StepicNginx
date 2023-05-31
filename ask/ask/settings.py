bbb8940b6a51d3c5a3af2d2642d66e0f2be20449
denis <denis1489@list.ru>
2023-05-31 11:05:23 +0300

    [upd] allowed hosts

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3m+z7opg=mm%tf@dvv9pss*3l#d2mb*jh6&4q24=6@(ycfiikj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
